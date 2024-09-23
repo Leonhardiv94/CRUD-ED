e46b85eff6e5b6f7ea685db089c9d3b28d5f9a2f
Leonhard <leonhardiv94@gmail.com>
2024-09-23 17:15:41 -0500

    avance

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { EmpleadosComponent } from './empleados/empleados.component';
+import { DepartamentosComponent } from './departamentos/departamentos.component';
+import { FormsModule } from '@angular/forms'; // Asegúrate de importar FormsModule
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EmpleadosComponent, DepartamentosComponent, FormsModule], // Combina todas las importaciones aquí
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Cambia 'styleUrl' a 'styleUrls'
 })
 export class AppComponent {
   title = 'frontend';
