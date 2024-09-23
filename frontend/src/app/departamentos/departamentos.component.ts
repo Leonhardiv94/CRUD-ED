7bca7714c3e702838a29317651843c47a7bdfd00
Leonhard <leonhardiv94@gmail.com>
2024-09-23 11:57:20 -0500

    avance

--- frontend/src/app/departamentos/departamentos.component.ts
+++ frontend/src/app/departamentos/departamentos.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DepartamentoService, Departamento } from '../departamento.service'; // Asegúrate de importar la interfaz Departamento
 
 @Component({
-  selector: 'app-departamentos',
-  standalone: true,
-  imports: [],
-  templateUrl: './departamentos.component.html',
-  styleUrl: './departamentos.component.css'
+    selector: 'app-departamentos',
+    templateUrl: './departamentos.component.html',
+    styleUrls: ['./departamentos.component.css']
 })
-export class DepartamentosComponent {
+export class DepartamentosComponent implements OnInit {
+    departamentos: Departamento[] = []; // Especifica el tipo como Departamento[]
+    nuevoDepartamento: Departamento = { codigo: 0, nombre: '' }; // Inicializa con 0
 
+    constructor(private departamentoService: DepartamentoService) { }
+
+    ngOnInit(): void {
+        this.cargarDepartamentos();
+    }
+
+    cargarDepartamentos() {
+        this.departamentoService.getDepartamentos().subscribe((data: Departamento[]) => {
+            this.departamentos = data;
+        });
+    }
+
+    agregarDepartamento() {
+        this.departamentoService.addDepartamento(this.nuevoDepartamento).subscribe(() => {
+            this.cargarDepartamentos();
+            this.nuevoDepartamento = { codigo: 0, nombre: '' }; // Resetear formulario
+        });
+    }
+
+    eliminarDepartamento(id: string) { // Especifica el tipo de id si es un string
+        this.departamentoService.deleteDepartamento(id).subscribe(() => {
+            this.cargarDepartamentos();
+        });
+    }
 }
--- frontend/src/app/empleados/empleados.component.ts
+++ frontend/src/app/empleados/empleados.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmpleadoService, Empleado } from '../empleado.service'; // Asegúrate de importar la interfaz Empleado
 
 @Component({
-  selector: 'app-empleados',
-  standalone: true,
-  imports: [],
-  templateUrl: './empleados.component.html',
-  styleUrl: './empleados.component.css'
+    selector: 'app-empleados',
+    templateUrl: './empleados.component.html',
+    styleUrls: ['./empleados.component.css']
 })
-export class EmpleadosComponent {
+export class EmpleadosComponent implements OnInit {
+    empleados: Empleado[] = []; // Especifica el tipo como Empleado[]
+    nuevoEmpleado: Empleado = { codigo: 0, nombre: '', apellido1: '', apellido2: '', codigo_departamento: 0 }; // Inicializa con 0
 
+    constructor(private empleadoService: EmpleadoService) { }
+
+    ngOnInit(): void {
+        this.cargarEmpleados();
+    }
+
+    cargarEmpleados() {
+        this.empleadoService.getEmpleados().subscribe((data: Empleado[]) => {
+            this.empleados = data;
+        });
+    }
+
+    agregarEmpleado() {
+        this.empleadoService.addEmpleado(this.nuevoEmpleado).subscribe(() => {
+            this.cargarEmpleados();
+            this.nuevoEmpleado = { codigo: 0, nombre: '', apellido1: '', apellido2: '', codigo_departamento: 0 }; // Resetear formulario
+        });
+    }
+
+    eliminarEmpleado(id: string) { // Especifica el tipo de id si es un string
+        this.empleadoService.deleteEmpleado(id).subscribe(() => {
+            this.cargarEmpleados();
+        });
+    }
 }
